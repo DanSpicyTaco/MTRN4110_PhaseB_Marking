4c1a8957594b27cf2e009d3394b39ea5d3fe1e35
Daniel Latimer <latdani@gmail.com>
2020-06-30 11:25:58 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# MTRN4110 Phase B Marking Script
+
+This is the marking script for Phase B.
+
+## Requirements
+
+- [Python 3.8](https://www.python.org/downloads/)
+
+## Usage
+
+1. Copy and paste the student's answer in `answer.txt`.
+   Make sure the leading `[<CONTROLLER_NAME>]` is removed from every line.
+2. Run this command in terminal
+
+```
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,146 @@
+import os
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+# Generate our own answer
+os.system("./sln > solution.txt")
+
+
+###
+# Task A: check that input and map match
+###
+def taskA(solution, answer):
+    # Get our solution's map, without outer walls
+    soln_map = []
+    if(solution.readline().strip() == "Start - Read map:"):
+        line = solution.readline().strip()
+        while line != "Done - Map read!":
+            soln_map.append(line)
+            line = solution.readline().strip()
+
+    # Get the student's map
+    stu_map = []
+    if(answer.readline().strip() == "Start - Read map:"):
+        line = answer.readline().strip()
+        while line != "Done - Map read!":
+            stu_map.append(line)
+            line = answer.readline().strip()
+
+    # Check if they're the same
+    correct_rows = 0
+    for i, row in enumerate(soln_map):
+        if(row == stu_map[i]):
+            correct_rows += 1
+        else:
+            print(f'{bcolors.FAIL} Row {i} is incorrect! {bcolors.ENDC}')
+            print(f'\tExpected:\n\t{row}\n\tGot:\n\t{stu_map[i]}\n')
+
+    if(correct_rows == 11):
+        # Full marks
+        a_mark = 10
+        colour = bcolors.OKGREEN
+    elif(correct_rows > 0):
+        # Maximum mark of 8
+        a_mark = correct_rows * 2
+        if(correct_rows > 8):
+            a_mark = 8
+        colour = bcolors.WARNING
+    else:
+        # Hardcoded or just incorrect map
+        a_mark = 0
+        colour = bcolors.FAIL
+
+    print(f'{colour} Task A: {a_mark}/10 {bcolors.ENDC}')
+
+###
+# Task B: check all paths are provided
+###
+
+
+def taskB(solution, answer):
+    # Throw away first line - should be "Start - Find shortest paths:"
+    solution.readline()
+    answer.readline()
+
+    # Get all the shortest paths printed out in the solution
+    soln_paths = []
+    line = solution.readline().strip()
+    while line != "Done - Find shortest paths":
+        # New path has started
+        if "start path -" in line:
+            soln_path = []
+        elif "done path -" in line:
+            soln_paths.append(soln_path)
+        else:
+            soln_path.append(line)
+
+        line = solution.readline().strip()
+
+    # Get all the shortest paths printed out in the student answer
+    stu_paths = []
+    line = answer.readline().strip()
+    while line != "Done - Find shortest paths":
+        # New path has started
+        if "start path -" in line:
+            stu_path = []
+        elif "done path -" in line:
+            stu_paths.append(stu_path)
+        else:
+            stu_path.append(line)
+
+        line = answer.readline().strip()
+
+    # Find the correct paths (in RIP time complexity)
+    correct_paths = 0
+    for i, soln_path in enumerate(soln_paths):
+        incorrect_path = False
+        stu_path = stu_paths[i]
+        for j, soln_row in enumerate(soln_path):
+            stu_row = stu_path[j]
+            if soln_row != stu_row:
+                incorrect_path = True
+                print(
+                    f'{bcolors.FAIL} Path {i}, Row {i} is incorrect! {bcolors.ENDC}')
+                print(f'\tExpected:\n\t{soln_row}\n\tGot:\n\t{stu_row}\n')
+
+        if incorrect_path == False:
+            correct_paths += 1
+
+    # Calculate the mark for this phase
+    if correct_paths == len(soln_paths):
+        # Full marks
+        b_mark = 50
+        colour = bcolors.OKGREEN
+    elif correct_paths > 0:
+        # TODO - figure out how to calculate this!
+        # Partial marks
+        b_mark = correct_paths
+        colour = bcolors.OKGREEN
+    else:
+        # No marks
+        b_mark = 0
+        colour = bcolors.FAIL
+
+    print(f'{colour} Task B: {b_mark}/50 {bcolors.ENDC}')
+
+# TODO
+# Task C: check the least turns path is valid
+
+# TODO
+# Task D: given the absolute path of the file, check if the path is correct
+
+
+with open("solution.txt", "r") as solution:
+    with open("answer.txt", "r") as answer:
+        taskA(solution, answer)
+        taskB(solution, answer)
