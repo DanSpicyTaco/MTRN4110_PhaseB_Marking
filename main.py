c331324a2e7dcb8e95571a3a2cf2f5ff4858d8e5
Daniel Latimer <latdani@gmail.com>
2020-07-03 13:10:15 +1000

    Added task 3

--- main.py
+++ main.py
@@ -11,28 +11,60 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+###
+# Helpers for all tasks
+###
 
-# Generate our own answer
-os.system("./sln > solution.txt")
 
+def print_map(map):
+    for row in map:
+        print(f'{row}\n')
+
+
+def compare_maps(soln_map, stu_map):
+    # Compare current stu_path with current soln_path
+    for i, soln_row in enumerate(soln_map):
+        if soln_row != stu_map[i]:
+            return False
+
+    return True
+
+
+def validate_map(m, steps=None, path=None):
+    print_map(m)
+    if steps is not None:
+        print(f'Steps: {steps}')
+    if path is not None:
+        print(f'Path: {path}')
+
+    ans = input("Is this map valid? [y/Y/n/N] ")
+
+    while True:
+        if ans == "y" or ans == "n":
+            break
+        ans = input("Please enter y/Y/n/N ")
+
+    return True if ans.lower() == "y" else False
 
 ###
 # Task A: check that input and map match
 ###
+
+
 def taskA(solution, answer):
     # Get our solution's map, without outer walls
     soln_map = []
-    if(solution.readline().rstrip() == "Start - Read map:"):
+    if(solution.readline().rstrip() == "--- Task 1 ---"):
         line = solution.readline().rstrip()
-        while line != "Done - Map read!":
+        while line != "--- Task 2 ---":
             soln_map.append(line)
             line = solution.readline().rstrip()
 
     # Get the student's map
     stu_map = []
-    if(answer.readline().rstrip() == "Start - Read map:"):
+    if(answer.readline().rstrip() == "--- Task 1 ---"):
         line = answer.readline().rstrip()
-        while line != "Done - Map read!":
+        while line != "--- Task 2 ---":
             stu_map.append(line)
             line = answer.readline().rstrip()
 
@@ -67,57 +99,48 @@ def taskA(solution, answer):
 ###
 
 
-def printMap(map):
-    for row in map:
-        print(f'{row}\n')
+def get_paths(f):
+    first_path = True
+    paths = []
+    line = f.readline().rstrip()
+    while line != "--- Task 3 ---":
+        # New path has started
+        if "--- Path" in line:
+            if first_path == True:
+                # Ignore the first "--- Path" we encounter - path will be []
+                first_path = False
+            else:
+                paths.append(path)
+
+            path = []
+        else:
+            path.append(line)
+
+        line = f.readline().rstrip()
+    return paths
 
 
-def compare_maps(soln_map, stu_map):
-    # Compare current stu_path with current soln_path
-    for i, soln_row in enumerate(soln_map):
-        if soln_row != stu_map[i]:
-            return False
+def remove_duplicate_paths(paths):
+    # Just remove duplicates manually
+    new_paths = []
+    for path in paths:
+        if path not in new_paths:
+            new_paths.append(path)
 
-    return True
+    return new_paths
 
 
 def taskB(solution, answer):
     #! Note: this removes duplicate shortest paths from the students answer
-    # Throw away first line - should be "Start - Find shortest paths:"
-    solution.readline()
-    answer.readline()
 
     # Get all the shortest paths printed out in the solution
-    soln_paths = []
-    line = solution.readline().rstrip()
-    while line != "Done - Find shortest paths":
-        # New path has started
-        if "start path -" in line:
-            soln_path = []
-        elif "done path -" in line:
-            soln_paths.append(soln_path)
-        else:
-            soln_path.append(line)
-
-        line = solution.readline().rstrip()
+    soln_paths = get_paths(solution)
 
     # Get all the shortest paths printed out in the student answer
-    stu_paths = []
-    line = answer.readline().rstrip()
-    while line != "Done - Find shortest paths":
-        # New path has started
-        if "start path -" in line:
-            stu_path = []
-        elif "done path -" in line:
-            stu_paths.append(stu_path)
-        else:
-            stu_path.append(line)
-
-        line = answer.readline().rstrip()
+    stu_paths = get_paths(answer)
 
     # Remove duplicate paths in stu_paths
-    # Copied from SO (lol)
-    stu_paths = sorted(map(list, set(map(tuple, stu_paths))))
+    stu_paths = remove_duplicate_paths(stu_paths)
 
     # Find the correct paths (in RIP time complexity)
     actual_correct_paths = len(soln_paths)
@@ -146,15 +169,7 @@ def taskB(solution, answer):
             f'{len(stu_paths)} paths in the student answer are either invalid or not the shortest path')
 
         for path in stu_paths:
-            printMap(path)
-            ans = input("Is this path valid? [y/Y/n/N] ")
-
-            while True:
-                if ans == "y" or ans == "n":
-                    break
-                ans = input("Please enter y/Y/n/N ")
-
-            if ans.lower() == "y":
+            if validate_map(path):
                 valid_paths += 1
 
     # Calculate the mark for this phase
@@ -190,19 +205,108 @@ def taskB(solution, answer):
 
     print(f'{colour} Task B: {b_mark}/50 {bcolors.ENDC}')
 
-# TODO
+
 ###
 # Task C: check the least turns path is valid
 ###
 
-# TODO
+
+def get_paths_and_steps(f):
+    paths = []
+    m = []
+    line = f.readline().rstrip()
+
+    while line != "--- Task 4 ---":
+        if "Path: " in line:
+            path = line.replace("Path: ", "")
+            paths.append({
+                "map": m,
+                "path": path,
+                "steps": steps,
+            })
+            m = []
+        elif "Steps: " in line:
+            steps = line.replace("Steps: ", "")
+        else:
+            m.append(line)
+
+        line = f.readline().rstrip()
+
+    return paths
+
+
+def taskC(solution, answer):
+    # Get all possible maps, paths and steps from the solution
+    soln_paths = get_paths_and_steps(solution)
+
+    # Get the least turns map from the student answer
+    stu_paths = get_paths_and_steps(answer)
+
+    if len(stu_paths) == 0:
+        # No attempt made
+        print(f'{bcolors.FAIL} Task C: 0/50 {bcolors.ENDC}')
+        return
+
+    stu_map = stu_paths[0]["map"]
+    stu_steps = stu_paths[0]["steps"]
+    stu_path = stu_paths[0]["path"]
+    c_mark = 0
+
+    for i in soln_paths:
+        soln_map = i["map"]
+        soln_steps = i["steps"]
+        soln_path = i["path"]
+
+        # Found a matching maze!
+        if compare_maps(soln_map, stu_map):
+            # Double check the path and steps are the same as well
+            if soln_steps == stu_steps:
+                if soln_path == stu_path:
+                    # Everything is correct!
+                    c_mark += 30
+                else:
+                    print(
+                        f'{bcolors.FAIL} Wrong path. Got {stu_path}. Expected {soln_path} {bcolors.ENDC}')
+                    # +5% for every 3 correct steps (consecutive)
+                    for s_index, s in enumerate(stu_path):
+                        if s == soln_path[s_index]:
+                            if s_index % 3 == 0:
+                                c_mark += 5
+                        else:
+                            break
+            else:
+                # +10 for finding a shortest path with least turns
+                print(
+                    f'{bcolors.FAIL} Wrong number of steps. Got {stu_steps}. Expected {soln_steps} {bcolors.ENDC}')
+                c_mark += 10
+
+    # No match found - manually check if it is correct
+    if c_mark == 0:
+        print(
+            f'Least turns path in the student answer is either invalid or not the shortest path')
+        print(f'For reference, expecting {soln_steps} turns in the path')
+        if validate_map(stu_map, steps=stu_steps, path=stu_path):
+            c_mark += 20
+
+    colour = bcolors.OKGREEN if c_mark == 30 else bcolors.WARNING
+
+    print(f'{colour} Task C: {c_mark}/30 {bcolors.ENDC}')
+
+
 ###
 # Task D: given the absolute path of the file, check if the path is correct
 ###
 
+def taskD(solution, answer):
+    pass
+
+# Generate our own answer
+os.system("./sln > solution.txt")
 
 # This is where the actual marking happens
 with open("solution.txt", "r") as solution:
     with open("answer.txt", "r") as answer:
         taskA(solution, answer)
         taskB(solution, answer)
+        taskC(solution, answer)
+        taskD(solution, answer)
